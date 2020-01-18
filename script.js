e24cebd9880ef1ead6ec3b3d231794cf927b34ec
Leonardo Celso de Souza <leonardo_souzac@hotmail.com>
2020-01-18 18:36:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function contar() {
+    let ini = document.getElementById('num')
+    let fim = document.getElementById('num2')
+    let passo = document.getElementById('num3')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert ('ERRO. Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            //Contagem regressiva
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+
+}
